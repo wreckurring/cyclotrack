8bf2480f9532f01d6161176e24a2ba7ca989399f
wreckurring <wreckurring@gmail.com>
2026-03-11 13:44:21 +0530

    ui setup

---
+++ dashboard/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './src/pages/Login';
+import LeaderDashboard from './src/pages/LeaderDashboard';
+import CyclistDashboard from './src/pages/CyclistDashboard';
+import './src/index.css'; //
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/leader" element={<LeaderDashboard />} />
+        <Route path="/cyclist" element={<CyclistDashboard />} />
+        {/* Fallback to login if route doesn't exist */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
