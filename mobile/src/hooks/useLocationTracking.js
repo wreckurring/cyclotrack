bd16dcc9c14ceedc73b710060b630ec9a9aced28
wreckurring <wreckurring@gmail.com>
2026-03-21 23:50:16 +0530

    .

--- mobile/src/hooks/useLocationTracking.js
+++ mobile/src/hooks/useLocationTracking.js
@@ -4,6 +4,33 @@ import * as Location from 'expo-location';
 export const useLocationTracking = (isActive, callback) => {
   const [error, setError] = useState(null);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   useEffect(() => {
     let subscriber;
     const startTracking = async () => {
